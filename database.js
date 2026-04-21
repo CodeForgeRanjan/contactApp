743c571143b022f6934a183aae6a3ead3365eee0
Rahul Ranjan <144208346+Rahul1398-sudo@users.noreply.github.com>
2026-04-21 14:47:22 +0530

    Refactor database connection code
    changes

--- database.js
+++ database.js
@@ -8,10 +8,3 @@ import mongoose from "mongoose";
 export default connectDB;
 
 
-// NOTE -> Agar mai const ke aage export likhta hu to mujhe server.js me import krte waqt ( impot {connectDB}) krna hoga nhi to mai direct export default connectDB krdunga to isse v kaam ho jayega.
-
-// const connectDB = () => {
-//   mongoose
-//   .connect("mongodb://127.0.0.1:27017/contacts-crud")
-//   .then(() => console.log("Database successfully connected"));
-// };
