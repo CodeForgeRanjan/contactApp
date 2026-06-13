7874b9ef3b3208b4a09f887126406630eb5e8013
Rahul_Ranjan <144208346+CodeForgeRanjan@users.noreply.github.com>
2026-06-13 14:15:35 +0530

    Reformat server.js for consistency

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 import ContactRoutes from "./routes/contact.route.js";
 import connectDB from "./config/database.js";
 
-//Middle-ware-> taaki html file ko read kar sake
-app.set("view engine", "ejs"); // EJS templating
-// app.use(express.json());
 
-app.use(express.urlencoded({ extended: false })); // To read form data
-//form ke data ko accept karne ke lye as a request
+app.set("view engine", "ejs"); 
+
+
+app.use(express.urlencoded({ extended: false })); 
+
 app.use(express.static("public"));
-// To serve static files like CSS, JS, Images
+
 
 //Routes
 app.use(ContactRoutes);
